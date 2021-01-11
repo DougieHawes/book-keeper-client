2615a1114ab6bfe9d3038ddeaed9dd238aaa4d84
dougiehawes <dougiehawes@hotmail.com>
2021-01-11 16:17:40 +0000

    applied redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Provider } from "react-redux";
+
+import store from "./redux/store/store";
+
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
